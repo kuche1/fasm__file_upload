0d5a786964ffd0bee3f587659a08e3c7d6eb0cde
kuche1 <znamsiparolata4esno@abv.bg>
2023-10-09 18:32:48 +0300

    update

--- file-transfer-https-wrapper.py
+++ file-transfer-https-wrapper.py
@@ -12,26 +12,32 @@ LISTEN = 5
 
 # speed limiter
 
-DOWNLOAD_SPEED = 1024 * 1024 * 5 # 5 MiB/s
-DOWNLOAD_SLEEP = 0.05 # seconds
-DOWNLOAD_CHUNK = int(DOWNLOAD_SPEED * DOWNLOAD_SLEEP)
+# REDIRECT_LOOP_SPEEP = 0.01
+SPEED_LIMIT_SLEEP = 0.05 # seconds
 
-UPLOAD_SPEED = 1024 * 1024 * 5 # 5 MiB/s
-UPLOAD_SLEEP = 0.05 # seconds
-UPLOAD_CHUNK = int(UPLOAD_SPEED * UPLOAD_SLEEP)
+# certificates
 
 HERE = os.path.dirname(os.path.realpath(__file__))
 SSL_KEYFILE  = os.path.join(HERE, 'certs', 'private.key')
 SSL_CERTFILE = os.path.join(HERE, 'certs', 'certificate.crt')
 
-def redirect_traffic(con, addr, server_port):
+def redirect_traffic(con, addr, server_port, speed_limit):
     try:
-        redirect_traffic2(con, addr, server_port)
+        redirect_traffic2(con, addr, server_port, speed_limit)
     finally:
-        con.shutdown(socket.SHUT_RDWR)
+
+        try:
+            con.shutdown(socket.SHUT_RDWR)
+        except OSError:
+            pass
+
         con.close()
 
-def redirect_traffic2(client, client_addr, server_port):
+def redirect_traffic2(client, client_addr, server_port, speed_limit):
+    speed_limit *= 1024 * 1024
+    download_chunk = int(speed_limit * SPEED_LIMIT_SLEEP)
+    upload_chunk = int(speed_limit * SPEED_LIMIT_SLEEP)
+
     client.setblocking(False)
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -45,13 +51,13 @@ def redirect_traffic2(client, client_addr, server_port):
         
         if time.time() >= next_download:
             try:
-                data = client.recv(DOWNLOAD_CHUNK)
+                data = client.recv(download_chunk)
             except BlockingIOError:
                 pass
             except ssl.SSLWantReadError:
                 pass
             else:
-                next_download = time.time() + DOWNLOAD_SLEEP * (len(data) / DOWNLOAD_CHUNK)
+                next_download = time.time() + SPEED_LIMIT_SLEEP * (len(data) / download_chunk)
 
                 server.setblocking(True)
                 server.sendall(data)
@@ -59,37 +65,34 @@ def redirect_traffic2(client, client_addr, server_port):
 
         if time.time() >= next_upload:
             try:
-                data = server.recv(UPLOAD_CHUNK)
+                data = server.recv(upload_chunk)
             except BlockingIOError:
                 pass
             else:
                 if len(data) == 0:
                     break
 
-                next_upload = time.time() + UPLOAD_SLEEP * (len(data) / UPLOAD_CHUNK)
+                next_upload = time.time() + SPEED_LIMIT_SLEEP * (len(data) / upload_chunk)
 
                 client.setblocking(True)
                 client.sendall(data)
                 client.setblocking(False)
-                time.sleep(UPLOAD_SLEEP)
+                time.sleep(SPEED_LIMIT_SLEEP)
         
-        # TODO ideally there would be a sleep here
+        # time.sleep(REDIRECT_LOOP_SPEEP)
 
     print('done')
 
-def main_connection_accepter(sock, server_port):
+def main_connection_accepter(sock, server_port, speed_limit):
     while True:
         try:
-            main_connection_accepter2(sock, server_port)
+            while True:
+                conn, addr = sock.accept()
+                threading.Thread(target=redirect_traffic, args=[conn, addr, server_port, speed_limit]).start()
         except:
             pass
 
-def main_connection_accepter2(sock, server_port):
-    while True:
-        conn, addr = sock.accept()
-        threading.Thread(target=redirect_traffic, args=[conn, addr, server_port]).start()
-
-def main(server_port, wrapper_port):
+def main(server_port, wrapper_port, speed_limit):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock = ssl.wrap_socket(
@@ -102,7 +105,7 @@ def main(server_port, wrapper_port):
     sock.bind(('', wrapper_port))
     sock.listen(LISTEN)
 
-    threading.Thread(target=main_connection_accepter, args=[sock, server_port]).start()
+    threading.Thread(target=main_connection_accepter, args=[sock, server_port, speed_limit]).start()
 
     try:
         while True:
@@ -114,6 +117,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='ssl wrapper for non-ssl servers')
     parser.add_argument('server_port', type=int, help='port of non-ssl server')
     parser.add_argument('wrapper_port', type=int, help='port for the ssl wrapper')
+    parser.add_argument('speed_limit', type=float, help='upload and download speeds will each be limited to this number (MiB)')
     args = parser.parse_args()
 
-    main(args.server_port, args.wrapper_port)
+    main(args.server_port, args.wrapper_port, args.speed_limit)

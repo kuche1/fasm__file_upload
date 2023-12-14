80bec9ca9a2ee8b34fd109eb2252d472b452929b
me <znamsiparolata4esno@abv.bg>
2023-12-14 19:29:24 +0200

    update

--- resources/common.js
+++ resources/common.js
@@ -20,3 +20,22 @@ function ls(ls_callback){
         ls_callback(files)
     }
 }
+
+function translate_name(name, callback){
+    const Http = new XMLHttpRequest(); // it sucks that it's an XMLHttp, should compensate for that on the back end
+    const url = '/7' + name;
+    Http.open("POST", url);
+    Http.send();
+
+    Http.onreadystatechange = (event) => {
+        // console.log(event)
+        if (Http.readyState !== XMLHttpRequest.DONE) {
+            return
+        }
+
+        let response = Http.responseText
+        console.log(response)
+
+        callback(response)
+    }
+}
